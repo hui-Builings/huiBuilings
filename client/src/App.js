5bc2821865c3f8b7209bb0554b27f1c2016b92a2
liuhuihui <244701374@qq.com>
2018-12-06 17:48:31 +0800

    合并调整

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import './App.scss';
 import Header from "./container/Header";
 import Content from "./container/Content";
@@ -10,28 +9,7 @@ export default class App extends Component {
       <div className="app">
           <Header />
           <Content />
-=======
-
-import './App.scss';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Content from "./components/Content/Content";
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-
-          <Header />
-          <Content />
-          <Footer />
->>>>>>> a2a9dbd9100a647bb18e5525b752b0419416cb66
       </div>
     );
   }
 }
-<<<<<<< HEAD
-=======
-
-export default App;
->>>>>>> a2a9dbd9100a647bb18e5525b752b0419416cb66
--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-=======
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom'
-
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-    , document.getElementById('root'));
->>>>>>> a2a9dbd9100a647bb18e5525b752b0419416cb66
 registerServiceWorker();
